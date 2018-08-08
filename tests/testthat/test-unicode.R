526f0323703f58ead9646bd221075b39619600fd
Timothy Mastny <tim.mastny@gmail.com>
2018-08-08 10:29:31 -0500

    add unicode tests

---
+++ tests/testthat/test-unicode.R
@@ -0,0 +1,28 @@
+context('unicode')
+
+test_that('unicode variables work', {
+  expected <- paste0(readLines("test-unicode-var-expected.css"), collapse = '\n')
+  class(expected) <- c('css', class(expected))
+
+  css <- compile('test-unicode-var-input.scss')
+
+  expect_equal(css, expected)
+})
+
+test_that('unicode css works', {
+  expected <- paste0(readLines("test-unicode-css-expected.css"), collapse = '\n')
+  class(expected) <- c('css', class(expected))
+
+  css <- compile('test-unicode-css-input.scss')
+
+  expect_equal(css, expected)
+})
+
+test_that('unicode bom', {
+  expected <- paste0(readLines("test-unicode-bom-expected.css"), collapse = '\n')
+  class(expected) <- c('css', class(expected))
+
+  css <- compile('test-unicode-bom-input.scss')
+
+  expect_equal(css, expected)
+})
