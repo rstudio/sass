efdb12a63b994eab1787b5b41c30d0f7192f558a
Barret Schloerke <schloerke@gmail.com>
2018-09-06 16:05:16 -0400

    add a rhub script
    results do not have much success

---
+++ scripts/rhub.R
@@ -0,0 +1,22 @@
+
+if (!require("rhub", quietly = TRUE)) install.packages("rhub")
+
+dir.create("../builds", recursive = TRUE, showWarnings = FALSE)
+build_file <- devtools::build(".", "../builds")
+
+platforms <- c("windows-x86_64-release", rhub:::default_cran_check_platforms(build_file))
+
+check_output <- rhub::check_for_cran(
+  build_file,
+  email = "barret@rstudio.com",
+  platforms = platforms,
+  show_status = FALSE
+)
+
+for (i in seq_along(platforms)) {
+  check_output$livelog(i)
+}
+
+# check_output$web()
+
+print(check_output)
