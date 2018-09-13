9732265d3dee8815758ae3ea361fe91df8c30e80
Barret Schloerke <schloerke@gmail.com>
2018-09-13 11:18:25 -0400

    make sure to install the package before compiling the docs

--- scripts/build_docs.R
+++ scripts/build_docs.R
@@ -2,8 +2,22 @@
 
 
 
+(function() {
+
+owd <- getwd()
+on.exit({
+  setwd(owd)
+})
+
+setwd(devtools::as.package(".")$path)
+
 devtools::document()
+
+devtools::install()
 
 rmarkdown::render("README.Rmd")
 
+unlink("docs", recursive = TRUE)
 pkgdown::build_site()
+
+})()
