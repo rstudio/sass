1087a20d41ea798640a6c60daa8ec97ec4eb5d15
Carson <cpsievert1@gmail.com>
2021-04-13 12:07:13 -0500

    update unit test

--- tests/testthat/test-cache.R
+++ tests/testthat/test-cache.R
@@ -179,7 +179,7 @@ test_that("output_template() is cache and options aware", {
 
   # File can be written in another dir and still be cache aware
   temp_dir <- withr::local_tempdir()
-  output7 <- sass(input, output = output_template(tmpdir = temp_dir), options = opts)
+  output7 <- sass(input, output = output_template(path = temp_dir), options = opts)
   expect_true(basename(dirname(dirname(output7))) == basename(temp_dir))
   expect_true(basename(dirname(output7)) == basename(dirname(output1)))
 })
