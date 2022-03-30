dc70a2b097714bba6e36ecbd5213244b06d900c3
Dewey Dunnington <dewey@fishandwhistle.net>
2022-03-30 12:03:08 -0300

    ensure CXX is used to link the final shared object

--- src/compile.h
+++ src/compile.h
@@ -3,7 +3,15 @@
 
 #include <Rinternals.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 SEXP compile_file(SEXP file, SEXP options);
 SEXP compile_data(SEXP data, SEXP options);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
